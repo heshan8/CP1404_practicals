6583f03d43b6f08802cba65d92807793488c1855
jc429392 <tharindu.liyanage1@my.jcu.edu.au>
2022-09-29 15:28:13 +1000

    added question numbers for clarity

--- prac_01/loops.py
+++ prac_01/loops.py
@@ -7,19 +7,23 @@ for i in range(1, 21, 2):
     print(i, end=' ')
 print()
 
+# a
 for i in range(0, 100, 10):
     print(i, end=' ')
 print()
 
+# b
 for i in range(20, 1, -1):
     print(i, end=' ')
 print()
 
+# c
 star_count = int(input("Stars: "))
 for stars in range(star_count):
     print("*", end="")
 print()
 
+# d
 star_count = int(input("Stars: "))
 for i in range(1, star_count + 1):
     print("*" * i)
