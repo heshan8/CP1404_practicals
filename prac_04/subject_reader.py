c7761220022bfb14435b29a30007a0bae1c0fd4b
Heshan Liyanage <heshan988@gmail.com>
2022-10-10 20:07:34 +1000

    Removed redundant lines of code, added function to print using f strings.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,21 +9,25 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_subject_details(data):
+    for subject_details in data:
+        print(f" {subject_details[0]:1} is taught by {subject_details[1]:12} and has {subject_details[2]:3} students")
 
 
 main()
