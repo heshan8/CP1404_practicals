fb176ba380398e53adc3dbc7c4f19b16d153e653
Heshan Liyanage <heshan988@gmail.com>
2022-10-10 21:00:56 +1000

    Completed task #Basic list operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,22 @@
+"""program that outputs information about a list of user entered numbers """
+
+def main():
+    numbers = []
+    number_of_numbers = int(input("How many numbers: "))
+
+    for number in range(1, number_of_numbers + 1):
+        user_input = int(input(f"Enter number: "))
+        numbers.append(user_input)
+    print(f"Numbers Entered: {numbers}")
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The largest number is {max(numbers)}")
+    print(f"The smallest number is {min(numbers)}")
+    average = sum(numbers) / len(numbers)
+    print(f"The average of the numbers is {float(average):2}")
+
+
+main()
+
+
+
