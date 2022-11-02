b22c1bff67c1d661a1c2d48424c821dda68e97fe
Heshan Liyanage <heshan988@gmail.com>
2022-11-02 22:55:46 +1000

    Complete task Playing the Guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,10 +7,26 @@ def main():
     guitar_name = input("Guitar name: ")
     while guitar_name != "":
         guitar_year = int(input("Guitar year: "))
-        guitar_cost = int(input("Guitar cost: "))
+        guitar_cost = float(input("Guitar cost: $ "))
         new_guitar = [guitar_name, guitar_year, guitar_cost]
         guitars.append(new_guitar)
+        print(f"{new_guitar[0]} ({new_guitar[1]}) : ${new_guitar[2]} added.")
         guitar_name = input("Guitar name: ")
+
+    guitars.append(Guitar("Test Guitar", 2000, 5000.0))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+
+    if guitars:
+        guitars.sort()
+        print("These are my guitars")
+        for i, guitar in enumerate(guitars, start=1):
+            vintage = ""
+            if guitar.is_vintage():
+                vintage = "(Vintage)"
+            # max_length = max(len(guitars[0]) for guitar in guitars)
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage}")
+    else:
+        print("No guitars found!")
 
 
 main()
