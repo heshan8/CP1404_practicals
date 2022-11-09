95408133f5ea29fcc9e01c8dad513f2a5bdd0009
Heshan Liyanage <heshan988@gmail.com>
2022-11-09 22:31:30 +1000

    Update code to use new attribute "Pointer Arithmatic"

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -17,7 +17,8 @@ class ProgrammingLanguage:
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, " \
+               f"Pointer Arithmatic={self.pointer_arithmatic},"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -26,12 +27,13 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    c_plus_plus = ProgrammingLanguage("C++", "Static", False, 1983, True)
 
-    languages = [ruby, python, visual_basic]
-    print(python)
+    languages = [ruby, python, visual_basic, c_plus_plus]
+    print(c_plus_plus)
 
     print("The dynamically typed languages are:")
     for language in languages:
