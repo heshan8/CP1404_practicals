1e1c286853963383dd2249afab9b6e2f5ba62d6f
Heshan Liyanage <heshan988@gmail.com>
2022-11-10 00:39:45 +1000

    Add code to read file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,9 +1,10 @@
 """Program incomplete"""
-import datetime
+
+from project import Projects
 
 PROJECTS_FILENAME = 'projects.txt'
 MENU = "Menu:\n(L)oad projects\n(S)ave projects\n(D)isplay projects" \
-       "\n(F)ilter projects by date\n(A)dd new project\n(U)pdate project\n(Q)uit"
+       "\n(F)ilter projects by date\n(A)dd new project\n(U)pdate project\n(Q)uit\n>>> "
 
 
 def main():
@@ -26,3 +27,17 @@ def main():
         else:
             print("Invalid menu choice")
             menu_choice = input(MENU).upper()
+
+
+main()
+
+
+# def load_projects_file():
+projects = []
+with open(PROJECTS_FILENAME, 'r', encoding="utf-8-sig") as in_file:
+    for line in in_file:
+        parts = line.strip().split()
+        projects.append(Projects(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]))
+        print(parts)
+
+
