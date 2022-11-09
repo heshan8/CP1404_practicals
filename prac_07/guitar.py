34a4f64caca9f15a3e260290df61c21e9344dff2
Heshan Liyanage <heshan988@gmail.com>
2022-11-09 23:36:22 +1000

    complete task 2 "more guitars!"

---
+++ prac_07/guitar.py
@@ -0,0 +1,17 @@
+class Guitar:
+    """Guitar information"""
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name}, ({self.year}) : ${self.cost}"
+
+    def get_age(self, current_year=2022):
+        return current_year - self.year
+
+    def __lt__(self, other):
+        return self.year < other.year
+
--- prac_07/guitar_file_reader.py
+++ prac_07/guitar_file_reader.py
@@ -1,17 +1,34 @@
 from guitar import Guitar
 
+GUITAR_FILENAME = 'guitars.csv'
+
 
 def main():
-    """Read file of programming language details, save as objects, display."""
     guitars = []
-    # Open the file for reading
     in_file = open('guitars.csv', 'r')
     in_file.readline()
     for line in in_file:
         parts = line.strip().split(',')
-        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-        guitars.append(guitar)
-        print(guitar)
+        name = parts[0]
+        year = int(parts[1])
+        cost = float(parts[2])
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add)
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        print(guitars)
+        name = input("Name: ")
+
+    with open(GUITAR_FILENAME, 'w', encoding="utf-8-sig") as in_file:
+        for item in guitars:
+            print(*item, sep=',', file=in_file)
 
 
 main()
