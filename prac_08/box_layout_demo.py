9a698294aa1b1313c7da58796e41f117d2a49521
Heshan Liyanage <heshan988@gmail.com>
2022-11-16 21:51:28 +1000

    Add docstrings

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,16 +4,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build GUI"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Greet user by name when button is pressed"""
         # print("Greet")
         self.root.ids.output_label.text = "Hello"
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Reset text field and output label"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
