5461149540875240c5ae8fa83d0540c5bbe4a931
Heshan Liyanage <heshan988@gmail.com>
2022-11-30 21:07:08 +1000

    Complete do from scratch activity "Dynamic Labels"

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -7,19 +7,20 @@ from kivy.uix.label import Label
 class DynamicLabelsApp(App):
     status_text = StringProperty()
 
-     def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            self.name_to_last_name = {"Jack": "Nicholson", "Nina": "Myers", "Sam": "Goodman"}
+    # def __init__(self, **kwargs):
+    #        super().__init__(**kwargs)
+    #        self.name_to_last_name = {"Jack": "Nicholson", "Nina": "Myers", "Sam": "Goodman"}
 
-    def build(self):
-        self.title = "Dynamic Widgets"
-        self.root = Builder.load_file('dynamic_labels.kv')
-        self.create_widgets()
-        return self.root
+    # def build(self):
+    #     self.title = "Dynamic Widgets"
+    #     self.root = Builder.load_file('dynamic_labels.kv')
+    #     self.create_widgets()
+    #     return self.root
+    #
+    # def create_widgets(self):
+    #     for name in self.name_to_last_name:
+    #         temp_label = Label(text=name)
+    #         self.root.ids.main.add_widget(temp_label)
 
-    def create_widgets(self):
-        for name in self.name_to_last_name:
-            temp_label = Label(text=name)
-            self.root.ids.main.add_widget(temp_label)
 
 DynamicLabelsApp().run()
