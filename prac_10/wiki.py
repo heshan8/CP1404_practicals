6bf03f4350f1fe86d01dd188a110734a11d77b49
Heshan Liyanage <heshan988@gmail.com>
2022-11-30 22:29:46 +1000

    Add code to handle exception and to print page title, summary and URL.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,6 +4,13 @@ import wikipedia
 
 user_input = input("Enter page title or search phrase: ").title()
 while user_input != "":
-    print(wikipedia.summary(user_input))
-    user_input = input("Enter page title or search phrase: ").title()
+    try:
+        get_data = wikipedia.page(user_input)
+        print(f"Page title: {get_data.title}")
+        print(f"Page URL: {get_data.url}")
+        print(wikipedia.summary(user_input))
+        user_input = input("Enter page title or search phrase: ").title()
+
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(e.options)
 print("Good bye")
